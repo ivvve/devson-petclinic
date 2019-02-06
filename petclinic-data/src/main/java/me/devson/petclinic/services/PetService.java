b415bf0c56574f5d74529bada2127d07a13bf7ca
devson <sonyc5720@gmail.com>
2019-02-06 22:22:34 +0900

    service interface 생성. closes #5

---
+++ petclinic-data/src/main/java/me/devson/petclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package me.devson.petclinic.services;
+
+import me.devson.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
